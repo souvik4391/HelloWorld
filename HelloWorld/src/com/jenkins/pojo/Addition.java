ed52aec016ded0e39d5d44f7cff5094b249045cb
souvik4391 <souvik.manutd@gmail.com>
2020-08-17 23:37:59 +0530

    This should break the build

--- HelloWorld/src/com/jenkins/pojo/Addition.java
+++ HelloWorld/src/com/jenkins/pojo/Addition.java
@@ -2,8 +2,11 @@ package com.jenkins.pojo;
 
 public class Addition {
 	
+	AdditionImpl newObj = new AdditionImpl();
+	
 	public void add(){
   
+	
     int x = 10;
     int y = 5;
     System.out.println("Sum is : " + x+y);
