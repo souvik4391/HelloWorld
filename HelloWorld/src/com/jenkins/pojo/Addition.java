955af76569fe69f05b8450b0a14a966eee8dd434
souvik4391 <31988851+souvik4391@users.noreply.github.com>
2020-08-18 00:06:31 +0530

    Removed the incorrect object creation

--- HelloWorld/src/com/jenkins/pojo/Addition.java
+++ HelloWorld/src/com/jenkins/pojo/Addition.java
@@ -2,13 +2,10 @@ package com.jenkins.pojo;
 
 public class Addition {
 	
-	AdditionImpl newObj = new AdditionImpl();
-	
-	public void add(){
-  
-	
+public void add(){
+
     int x = 10;
     int y = 5;
     System.out.println("Sum is : " + x+y);
-    }
+  }
 }
